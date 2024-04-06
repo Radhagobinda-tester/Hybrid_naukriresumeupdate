bde87c227e8e7ac80c96f2040a644537c406a7d7
ADMIN <ADMIN@Radhagobinda.bbrouter>
2024-04-06 09:47:58 +0530

    zxzzxz

--- src/main/java/genericLib/BaseClass.java
+++ src/main/java/genericLib/BaseClass.java
@@ -6,9 +6,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
-
+import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
