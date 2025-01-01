476d5769787d0745cde56f59455ad9c5758595c3
ADMIN <ADMIN@Radhagobinda>
2025-01-01 22:57:43 +0530

    hhhjj

--- src/main/java/genericLib/BaseClass.java
+++ src/main/java/genericLib/BaseClass.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
